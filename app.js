b0aa87627e9d9065c907c0d2803a7c1a70901543
AlexAleshina <aleshina.aleksandra.1993@gmail.com>
2019-10-07 17:11:34 +0200

    new

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 let express = require('express');
 let app = express();
 
-const index = require('./routes/index');
-app.use('/'. index);
+const bodyParser   = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express      = require('express');
+const favicon      = require('serve-favicon');
+const hbs          = require('hbs');
+const mongoose     = require('mongoose');
+const logger       = require('morgan');
+const path         = require('path');
+var session        = require("express-session");
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+
+
+let index = require('./routes/index');
+app.use('/', index);
+
+let login = require('./routes/login');
+app.use('/', login);
+
+let signup = require('./routes/signup');
+app.use('/', signup);
+
+module.exports = app;
