7a3dec3afae32fe7442c51e1ea23abf7057b52d6
Mithila Fox <mithilafox@MitBook.local>
2019-10-08 16:06:56 +0200

    jack commit number 2

--- routes/auth.js
+++ routes/auth.js
@@ -12,7 +12,7 @@ router.get('/signup', (req, res, next) => {
 
 router.post('/signup', (req, res) => {
   const renderError = (msg) => {
-    console.log(msg); 
+    console.log(msg);
     res.render('signup', { error: msg });
   }
   console.log(req.body);
@@ -50,7 +50,7 @@ router.post('/signup', (req, res) => {
 router.get('/login', (req, res, next) => {
   res.render('login');
 });
-
+/*
 router.post('/login', (req, res, next) => {
   // 1. Check login name / password 
   // 2. If correct, save the user to the seesion
@@ -58,26 +58,27 @@ router.post('/login', (req, res, next) => {
   // and https://github.com/Piepongwong/auth-b6/blob/be3e5c8511cf495bca25d12107be29b28ef58aaf/app.js#L17 
   res.render('mytrips');
 });
+*/
 
-
-router.post("/login", (req,res)=> {
-  User.findOne({email: req.body.email})
-      .then((user)=> {
-          if(!user) res.json({loggedIn: false}) // this is different
+router.post("/login", (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) res.json({ loggedIn: false }) // this is different
+      else {
+        bcrypt.compare(req.body.password, user.passwordHash, function (err, equal) {
+          if (err) res.send(err);
+          else if (!equal) res.json({ loggedIn: false }); // this is different
           else {
-              bcrypt.compare(req.body.password, user.password, function(err, equal) {
-                  if(err) res.send(err);
-                  else if(!equal) res.json({loggedIn: false}); // this is different
-                  else {
-                      req.session.user = user;
-                      res.json({loggedIn: true}); // this is different
-                  }
-              });
+            req.session.user = user;
+            res.redirect('/mytrips')
+            //res.json({ loggedIn: true }); // this is different
           }
-      })
-      .catch(err=> {
-          res.send("error erropr", err);
-      })
+        });
+      }
+    })
+    .catch(err => {
+      res.send("error error", err);
+    })
 })
 
 //logout
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,26 @@ let router = express();
 
 router.get('/', (req, res, next) => {
     res.render('index');
-  });
+});
 
-  module.exports = router;
+router.get('/about', (req, res, next) => {
+    res.render('about');
+});
+
+router.get('/help', (req, res, next) => {
+    res.render('help');
+});
+
+router.get('/search', (req, res, next) => {
+    res.render('search');
+});
+
+router.get('/result', (req, res, next) => {
+    res.render('result');
+});
+
+router.get('/mytrips', (req, res, next) => {
+    res.render('mytrips');
+});
+
+module.exports = router;
