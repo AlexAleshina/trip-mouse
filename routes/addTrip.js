189b27e33ad74373d7a00ae0841b8807880d71c1
AlexAleshina <aleshina.aleksandra.1993@gmail.com>
2019-10-10 17:38:26 +0200

    searchChanges

--- routes/addTrip.js
+++ routes/addTrip.js
@@ -18,31 +18,54 @@ router.get('/add', (req, res, next) => {
 )
 */
 router.post('/add', (req, res) => {
+
     const renderError = (msg) => {
         console.log(msg);
         res.render('searchResult', { error: msg });
     }
     console.log(req.body);
 
-    Trip.findOne({})
-        .then((trip) => {
-            Trip.create({
-                origin: req.body.origin,
-                destination: req.body.destination,
-                value: req.body.value,
-                depart_date: req.body.depart_date,
-                return_date: req.body.return_date,
-                gate: req.body.gate
-            })
-                .then(() => {
+    Trip.create({
+        origin: req.body.origin,
+        destination: req.body.destination,
+        value: req.body.value,
+        depart_date: req.body.depart_date,
+        return_date: req.body.return_date,
+        gate: req.body.gate,
+        number_of_changes: req.body.number_of_changes
+
+    })
+        .then(() => {
+            Trip.find({})
+                .then((trip) => {
                     res.render('mytrips', { trip })
                 })
                 .catch((err) => {
                     renderError(err.message)
                 })
         })
-          })
+})
 
+
+
+
+//Trip.findOne({ value: req.body.value, depart_date: req.body.depart_date, origin: req.body.origin })
+
+
+
+router.get('/delete', (req, res, next) => {
+    debugger
+    Trip.findByIdAndRemove(req.query.deleteId)
+        .then(() => {
+            res.redirect(`/mytrips`)
+        })
+        .catch((err) => {
+            res.send(err)
+        })
+})
+
+
+module.exports = router
 
 
 module.exports = router;
